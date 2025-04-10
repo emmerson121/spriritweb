1fee7592da8d54937cd85bd7da65899ad2b82324
emmerson121 <emmanuelebiefie7@gmail.com>
2025-04-10 20:29:59 +0100

    first input

---
+++ src/feedback.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './feedback.css';
+
+const Feedback = () =>{
+
+    return(
+        <div>
+            <h1>Spirit Website</h1>
+        </div>
+    )
+}
+
+export default Feedback;g
